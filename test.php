2b0a051f5fe89052970f429b24af3a7d3971a24b
Bac LE VAN <baclv@rikkeisoft.com>
2016-12-13 10:34:53 +0700

    Update test.php
    Thực hiện viết lại class Test.php

--- test.php
+++ test.php
@@ -1,6 +1,9 @@
 <?php
-$arr = array(1, 2, 3, 4, 5);
-echo '<pre>';
-print_r($arr);
-echo '</pre>';
+Class Test {
+  public funtion build () {
+    $arr = array(1, 2, 3, 4, 5);
+    print_r($arr);
+    echo 'Thực hiện thành công';
+  }
+}
 ?>
